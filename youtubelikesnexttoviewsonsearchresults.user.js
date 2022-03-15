bb825b3ae8f3deccec8899a50b41996116371063
Florian9299 <florian.genilke@web.de>
2022-03-15 09:36:38 +0100

    finished

--- youtubelikesnexttoviewsonsearchresults.user.js
+++ youtubelikesnexttoviewsonsearchresults.user.js
@@ -5,7 +5,7 @@
 // @author       fflo92
 // @match        https://www.youtube.com/results*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
@@ -16,11 +16,50 @@
         return contents[0].children;
     }
 
+    var getViews = function(video) {
+        var metadata = video.querySelector('div#metadata[class="style-scope ytd-video-meta-block"]');
+        return metadata ? metadata.children[0] : null;
+    }
+
+    var getVideoLink = function(video) {
+        var title = video.querySelector('a#video-title');
+        var href = title.getAttribute('href');
+        return 'https://www.youtube.com' + href;
+    }
+    
+    var getPage = function(url, callback) {
+        GM_xmlhttpRequest({
+            method:     'GET',
+            url:        url,
+            onload:     callback
+        });
+    }
+
+    var getLikes = function(response) {
+        var pageSource = response.responseText;
+        var rightAnchor = ' likes"}';
+        var leftAnchor = '"label":"';
+        var rightIdx = pageSource.indexOf(rightAnchor);
+        var chunk = pageSource.substring(rightIdx-30, rightIdx);
+        var leftIdx = chunk.indexOf(leftAnchor);
+        return chunk.substring(leftIdx + leftAnchor.length)
+    }
+
+    var processVideo = function(video) {
+        var views = getViews(video);
+        if (views) {
+            var videoLink = getVideoLink(video);
+            getPage(videoLink, function(response) {
+                views.append(' '+ getLikes(response) + 'likes');
+            });
+        }
+    }
+
     var run = function() {
         var videos = getVideos();
         for (var i=0; i<videos.length; i++) {
             var video = videos[i];
-            console.log(video);
+            processVideo(video);
         }
     }
 
